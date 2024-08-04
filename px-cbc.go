8f2f47f917a6f19365b130eecbeaddb5e3a37aa6
Rafael R. Marques <rrmmbmg903@gmail.com>
2024-08-04 21:32:56 +0000

    feat: add AES-PX-CBC decryption implementation

--- px-cbc.go
+++ px-cbc.go
@@ -58,3 +58,53 @@ func (x *pxcbcEncrypter) SetIV(iv []byte) {
 	}
 	subtle.ConstantTimeCopy(1, x.iv, iv)
 }
+
+type pxcbcDecrypter struct {
+	block     cipher.Block
+	iv        []byte
+	blockSize int
+}
+
+func NewPXCBCDecrypter(block cipher.Block, iv []byte) cipher.BlockMode {
+	blockSize := block.BlockSize()
+	if len(iv) != blockSize {
+		panic("cipher.NewPXCBCDecrypter: IV length must equal block size")
+	}
+	return &pxcbcDecrypter{
+		block:     block,
+		iv:        iv,
+		blockSize: blockSize,
+	}
+}
+
+func (x *pxcbcDecrypter) BlockSize() int {
+	return x.blockSize
+}
+
+func (x *pxcbcDecrypter) CryptBlocks(dst []byte, src []byte) {
+	if len(src)%x.blockSize != 0 {
+		panic("input not full blocks")
+	}
+	if len(dst) < len(src) {
+		panic("output smaller than input")
+	}
+	iv := x.iv
+
+	for len(src) > 0 {
+		block := src[:x.blockSize]
+		src = src[x.blockSize:]
+
+		x.block.Decrypt(dst[:x.blockSize], block)
+		subtle.XORBytes(dst[:x.blockSize], dst[:x.blockSize], iv)
+		subtle.ConstantTimeCopy(1, iv, dst[:x.blockSize])
+		dst = dst[x.blockSize:]
+	}
+	subtle.ConstantTimeCopy(1, x.iv, iv)
+}
+
+func (x *pxcbcDecrypter) SetIV(iv []byte) {
+	if len(iv) != x.blockSize {
+		panic("cipher: incorrect length IV")
+	}
+	subtle.ConstantTimeCopy(1, x.iv, iv)
+}
